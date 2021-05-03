1011c6bf94608d1e89cba99d52831dda63cb3c67
Bryan <bryankoh137@hotmail.com>
2021-05-03 20:03:55 +0800

    fixed bugs with FileStorageController

--- webserver/src/main/kotlin/com/tempfiledrop/webserver/service/filestorage/FileStorageController.kt
+++ webserver/src/main/kotlin/com/tempfiledrop/webserver/service/filestorage/FileStorageController.kt
@@ -1,6 +1,8 @@
 package com.tempfiledrop.webserver.service.filestorage
 
 import org.slf4j.LoggerFactory
+import org.springframework.core.io.Resource
+import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
@@ -33,7 +35,7 @@ class FileStorageController(
         }
     }
 
-    @GetMapping("/files")
+    @GetMapping("/list")
     fun getAllFiles(): ResponseEntity<List<FileInfo>> {
         val fileInfos: List<FileInfo> = service.loadAllFilesFromFolder().map { path ->
             val filename: String = path.fileName.toString()
@@ -41,5 +43,12 @@ class FileStorageController(
             FileInfo(filename, url)
         }.collect(Collectors.toList())
         return ResponseEntity(fileInfos, HttpStatus.OK)
+    }
+
+    @GetMapping("/list/{filename:.+}")
+    @ResponseBody
+    fun getFile(@PathVariable filename: String): ResponseEntity<Resource> {
+        val file: Resource = service.loadFromFolder(filename)
+        return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + file.filename.toString() + "\"").body(file)
     }
 }
