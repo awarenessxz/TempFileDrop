7dd925a8504a23f1863f2eb83bff1309af2efd72
Bryan <bryankoh137@hotmail.com>
2021-05-10 23:57:53 +0800

    testing http-proxy-middleware

---
+++ webapp/src/setupProxy.js
@@ -0,0 +1,6 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = function(app) {
+    app.use(createProxyMiddleware("/api", { target: "http://localhost:7001", changeOrigin: true, secure: false }));
+    app.use(createProxyMiddleware("/api-docs", { target: "http://localhost:8001", changeOrigin: true, secure: false }));
+};
