374f3366e67d84e4b3bfdab10d8054d78896e3e7
Bryan <bryankoh137@hotmail.com>
2021-09-08 16:11:15 +0800

    fixed notification message for websocket

--- design4/storage-service/src/main/kotlin/com/tempstorage/storagesvc/service/notification/NotificationService.kt
+++ design4/storage-service/src/main/kotlin/com/tempstorage/storagesvc/service/notification/NotificationService.kt
@@ -4,6 +4,7 @@ import com.tempstorage.storagesvc.service.metadata.StorageMetadata
 import com.tempstorage.storagesvc.service.metadata.StorageMetadataService
 import org.springframework.messaging.simp.SimpMessagingTemplate
 import org.springframework.stereotype.Service
+import java.time.ZonedDateTime
 
 @Service
 class NotificationService(
@@ -12,20 +13,23 @@ class NotificationService(
         private val simpMessagingTemplate: SimpMessagingTemplate
 ) {
     fun triggerUploadNotification(storageMetadata: StorageMetadata) {
+        val message = NotificationMessage(ZonedDateTime.now(), EventType.FILES_UPLOADED, storageMetadata.bucket, storageMetadata.objectName)
         storageMetadataService.saveStorageMetadata(storageMetadata)                             // update database
-        producer.sendEventwithHeader(EventType.FILES_UPLOADED, storageMetadata)                 // notify (rabbitmq)
-        simpMessagingTemplate.convertAndSend("/topic/file-uploaded", storageMetadata) // notify (websocket)
+        producer.sendEventwithHeader(message)                                                   // notify (rabbitmq)
+        simpMessagingTemplate.convertAndSend("/topic/file-uploaded", message)         // notify (websocket)
     }
 
     fun triggerDeleteNotification(storageMetadata: StorageMetadata) {
+        val message = NotificationMessage(ZonedDateTime.now(), EventType.FILES_DELETED, storageMetadata.bucket, storageMetadata.objectName)
         storageMetadataService.deleteStorageMetadataByObjectName(storageMetadata.objectName)    // update database
-        producer.sendEventwithHeader(EventType.FILES_DELETED, storageMetadata)                  // notify
-        simpMessagingTemplate.convertAndSend("/topic/file-deleted", storageMetadata)  // notify (websocket)
+        producer.sendEventwithHeader(message)                                                   // notify
+        simpMessagingTemplate.convertAndSend("/topic/file-deleted", message)          // notify (websocket)
     }
 
     fun triggerDownloadNotification(storageMetadata: StorageMetadata) {
-        storageMetadataService.reduceDownloadCountByObjectName(storageMetadata.objectName)        // update database
-        producer.sendEventwithHeader(EventType.FILES_DOWNLOADED, storageMetadata)                 // notify
-        simpMessagingTemplate.convertAndSend("/topic/file-downloaded", storageMetadata) // notify (websocket)
+        val message = NotificationMessage(ZonedDateTime.now(), EventType.FILES_DOWNLOADED, storageMetadata.bucket, storageMetadata.objectName)
+        storageMetadataService.reduceDownloadCountByObjectName(storageMetadata.objectName)      // update database
+        producer.sendEventwithHeader(message)                                                   // notify
+        simpMessagingTemplate.convertAndSend("/topic/file-downloaded", message)       // notify (websocket)
     }
 }
--- design4/storage-service/src/main/kotlin/com/tempstorage/storagesvc/service/notification/RabbitMQProducer.kt
+++ design4/storage-service/src/main/kotlin/com/tempstorage/storagesvc/service/notification/RabbitMQProducer.kt
@@ -1,13 +1,11 @@
 package com.tempstorage.storagesvc.service.notification
 
 import com.tempstorage.storagesvc.service.eventdata.EventDataService
-import com.tempstorage.storagesvc.service.metadata.StorageMetadata
 import org.slf4j.LoggerFactory
 import org.springframework.cloud.stream.function.StreamBridge
 import org.springframework.messaging.MessageHeaders
 import org.springframework.messaging.support.MessageBuilder
 import org.springframework.stereotype.Component
-import java.time.ZonedDateTime
 
 @Component
 class RabbitMQProducer(
@@ -19,17 +17,15 @@ class RabbitMQProducer(
         private val logger = LoggerFactory.getLogger(RabbitMQProducer::class.java)
     }
 
-    fun sendEvent(eventType: EventType, storageMetadata: StorageMetadata) {
-        logger.info("Publishing Event ($eventType.name) to $STORAGE_SERVICE_CHANNEL")
-        val message = NotificationMessage(ZonedDateTime.now(), EventType.FILES_UPLOADED, storageMetadata.bucket, storageMetadata.objectName)
+    fun sendEvent(message: NotificationMessage) {
+        logger.info("Publishing Event (${message.eventType.name}) to $STORAGE_SERVICE_CHANNEL")
         streamBridge.send(STORAGE_SERVICE_CHANNEL, message)
         eventDataService.writeToDB(message)
     }
 
-    fun sendEventwithHeader(eventType: EventType, storageMetadata: StorageMetadata) {
-        val message = NotificationMessage(ZonedDateTime.now(), EventType.FILES_UPLOADED, storageMetadata.bucket, storageMetadata.objectName)
-        val routingKey = storageMetadata.bucket
-        logger.info("Publishing Event (${eventType.name}) to $STORAGE_SERVICE_CHANNEL with router Key ($routingKey)")
+    fun sendEventwithHeader(message: NotificationMessage) {
+        val routingKey = message.bucket
+        logger.info("Publishing Event (${message.eventType.name}) to $STORAGE_SERVICE_CHANNEL with router Key ($routingKey)")
         // publish
         streamBridge.send(STORAGE_SERVICE_CHANNEL, MessageBuilder.createMessage(
                 message,
